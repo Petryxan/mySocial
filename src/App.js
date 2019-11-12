53b2da0b27eaa68a8c3a304f8feef0b17cc3445d
Petryxan <petryxan@seznam.cz>
2019-11-12 21:53:38 +0100

    start

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+function App () {
+  return (
+    <div className="wrapper">
+      <h1>Social network</h1>
+      <h2>test223332</h2>
+    
+
+    </div>
+  )
+  
+}
+
+export default App;
+
