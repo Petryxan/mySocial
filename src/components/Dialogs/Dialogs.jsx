295e246532cef799ba7e4572808a66ea159f29a9
Petryxan <petryxan@seznam.cz>
2019-11-22 00:43:14 +0100

    add props in dialog and posts

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -20,15 +20,39 @@ const Message =(props)=> {
 }
 const Dialogs = (props) => {
 
+  let dialogsData = [
+    {id: 1, name: "Petro"},
+    {id: 2, name: "Ira"},
+    {id: 3, name: "Dima"}, 
+    {id: 4, name: "Oleg"},
+    {id: 5, name: "Julia"}, 
+    {id: 6, name: "Jura"},
+    
+  ]
+
+  let messagesData = [
+  {id: 1, message: "1111"},
+  {id: 2, message: "2222"},
+  {id: 3, message: "3333"},
+  {id: 4, message: "4444"},
+  {id: 5, message: "5555"},
+  {id: 6, message: "6666"},
+  ]
+    
+   
+  
+
   return (
   <div className={s.dialogs}>
      <div className={s.dialogsItems}>
-       <DialogItem name="Petro" id="1" />
+       {/* <DialogItem name="Petro" id="1" />
        <DialogItem name="Ira" id="2" />
        <DialogItem name="Dima" id="3" />
        <DialogItem name="Oleg" id="4" />
        <DialogItem name="Julia" id="5" />
-       <DialogItem name="Jura" id="6" />
+       <DialogItem name="Jura" id="6" /> */}
+
+       <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
 
        {/* Старий код де не було пропсів. Новий написаний вище.
        
@@ -50,10 +74,11 @@ const Dialogs = (props) => {
 
      </div>
 <div className={s.messages}>
-  <Message message="Hi" />
+  {/* <Message message="Hi" />
   <Message message="1111" />
   <Message message="2222" />
-  <Message message="3333" />
+  <Message message="3333" /> */}
+  <Message message={messagesData[0].message} />
   
 
 </div>
--- src/components/profile/MyPosts/MyPosts.jsx
+++ src/components/profile/MyPosts/MyPosts.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import s from "./MyPosts.module.css";
 import Post from "./Post/Post.jsx";
 
-const MyPosts = () => {
+const MyPosts = (props) => {
+
+  let postData = [
+    {id: 1, message: "Hi, How are you?)))))", likesCount: 12},
+    {id: 2, message: "its my first post))))", likesCount: 11},
+    {id: 3, message: "3333"},
+    {id: 4, message: "4444"},
+    {id: 5, message: "5555"},
+    {id: 6, message: "66661111"},
+    ]
+
   return (
     <div className={s.postsBlock}>
       <h3>My Posts</h3>
@@ -16,8 +26,8 @@ const MyPosts = () => {
         </div>
       </div>
       <div className={s.posts}>
-        <Post message="Hi, How are you?" likes=" 10" />
-        <Post message="its my first post" likes=" 20" />
+        <Post message={postData[0+].message} likes={postData[0].likesCount} />
+        <Post message={postData[1].message} likes={postData[1].likesCount} />
         <Post likes=" 30" message="2 post" />
         <Post likes=" 40" />
         <Post coment=" coment1" />
